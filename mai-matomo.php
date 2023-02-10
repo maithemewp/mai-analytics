8db4c9c9719f6598d85d1a28f2912768664742b5
David Schmeltzle <david@bizbudding.com>
2023-02-10 14:51:01 -0500

    bump version and updated includes

--- mai-matomo.php
+++ mai-matomo.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Matomo
  * Plugin URI:      https://bizbudding.com/
  * Description:     Matomo helper plugin for BizBudding/Mai Theme.
- * Version:         0.0.2
+ * Version:         0.0.3
  *
  * Author:          BizBudding
  * Author URI:      https://bizbudding.com
@@ -16,6 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 define ('bbmtm_DEBUG', true);
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/classes/bbmtm.php';
 
 /**
  * Instantiate the class.
