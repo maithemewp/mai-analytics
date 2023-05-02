b6bce7d04e3bf32bab6fc889cf23975e2e0f3f18
JiveDig <jivetheuniversal@gmail.com>
2023-05-02 11:47:39 -0400

    0.2.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '3527781e7e41af81808ec1c2d2c647f241412a9f',
+        'reference' => '73cde751ebbd43ce3ecf1ee6a4666d921cc598ac',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '3527781e7e41af81808ec1c2d2c647f241412a9f',
+            'reference' => '73cde751ebbd43ce3ecf1ee6a4666d921cc598ac',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
